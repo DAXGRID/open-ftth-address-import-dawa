391492aefe8f51a592162f9d6ff659851b652704
Rune Nielsen <runenielsen@runbox.com>
2022-07-14 11:51:58 +0200

    setup host

--- src/OpenFTTH.AddressIndexer.Dawa/Program.cs
+++ src/OpenFTTH.AddressIndexer.Dawa/Program.cs
@@ -1,9 +1,30 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
 namespace OpenFTTH.AddressIndexer.Dawa;
 
-public static class Program
+public sealed class Program
 {
-    public static void Main(string[] _)
+    public static async Task Main(string[] _)
     {
-        Console.WriteLine("Hello world!");
+        using var host = HostConfig.Configure();
+        var logger = host.Services.GetService<ILogger<Program>>();
+
+        if (logger is null)
+        {
+            throw new InvalidOperationException("Logger has not been configured.");
+        }
+
+        try
+        {
+            await host.StartAsync().ConfigureAwait(false);
+            await host.WaitForShutdownAsync().ConfigureAwait(false);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError("{}", ex.ToString());
+            throw;
+        }
     }
 }
---
+++ src/OpenFTTH.AddressIndexer.Dawa/Settings.cs
@@ -0,0 +1,5 @@
+namespace OpenFTTH.AddressIndexer.Dawa;
+
+internal sealed record Settings
+{
+}
